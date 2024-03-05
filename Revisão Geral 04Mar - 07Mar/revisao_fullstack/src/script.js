c9dbcc86e60b5fffbf332c2eedf939b50ab34688
Hugo Amadio <hugo_amadio@hotmail.com>
2024-03-05 15:04:37 -0300

    Atualizando exercicio revisão

--- Revisão Geral 04Mar - 07Mar/revisao_fullstack/src/script.js
+++ Revisão Geral 04Mar - 07Mar/revisao_fullstack/src/script.js
@@ -27,10 +27,54 @@ let novoNomeProduto = "Frigideira"
 let novoPreco = 75
 
 let index = listaProdutos.findIndex(produto => produto.nomeProduto === nomeProdutoAtualizar)
-console.log(index)
+
 listaProdutos[index].nomeProduto = novoNomeProduto
 listaProdutos[index].precoProduto = novoPreco
 
 listaProdutos.forEach(element => {
     console.log(element)
 });
+
+function criarProduto() {
+    let novoProduto2 = {
+        nomeProduto: "Banana",
+        precoProduto: 12
+    }
+    listaProdutos.push(novoProduto2)
+}
+
+function lerProduto() {
+    listaProdutos.forEach(element => {
+        console.log(element)
+    });
+}
+
+function atualizarProduto(){
+    let nomeProdutoAtualizar2 = "Banana"
+    let index = listaProdutos.findIndex(element => element.nomeProduto == nomeProdutoAtualizar2)
+    if(index != -1){
+        listaProdutos[index].nomeProduto = "Maça"
+        listaProdutos[index].precoProduto = 5
+    } else {
+        console.log("Produto a atualizar não encontrado!")
+    }
+
+}
+
+function deletarProduto(){
+    let nomeProdutoExcluir = "Frigideira"
+    let index = listaProdutos.findIndex(element => element.nomeProduto == nomeProdutoExcluir)
+    if(index != -1){
+        listaProdutos.splice(index, 1)
+    } else {
+        console.log("Produto a deletar não encontrado!")
+    }
+}
+
+console.log("----------------------------------------------------------")
+criarProduto()
+lerProduto()
+atualizarProduto()
+console.log("----------------------------------------------------------")
+deletarProduto()
+lerProduto()
