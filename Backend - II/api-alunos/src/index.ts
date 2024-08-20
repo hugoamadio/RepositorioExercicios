e66ff301c6307eaf1e34b9943013287ba0ff0464
Hugo Amadio <hugo_amadio@hotmail.com>
2024-08-20 13:18:58 -0300

    Subindo frontend react de axios com intermediadores de requisição http com token

--- Backend - II/api-alunos/src/index.ts
+++ Backend - II/api-alunos/src/index.ts
@@ -3,12 +3,14 @@ import * as dotenv from "dotenv";
 import studentsRoutes from "./routes/students.routes";
 import classroomsRoutes from "./routes/classrooms.routes"
 import registrationRoutes from "./routes/registration.routes"
+import cors from 'cors'
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 //ROUTES
 app.use("/students", studentsRoutes());
